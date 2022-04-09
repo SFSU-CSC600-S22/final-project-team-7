e923f3e40e311fcce1323ff69fdec0f44652fc3d
Eddy Yun <eyun1@mail.sfsu.edu>
2022-04-09 11:10:34 -0700

    visualizer done

--- client/src/visualizers/eyun1988.tsx
+++ client/src/visualizers/eyun1988.tsx
@@ -13,25 +13,18 @@ export const WaveformVisualizer1 = new Visualizer(
         const dim = Math.min(width, height);
 
         p5.background(0, 0, 0, 255);
-        // p5.background(250);
 
         p5.strokeWeight(dim * 0.01);
         p5.stroke(255, 255, 255, 255);
         p5.noFill();
 
         const values = analyzer.getValue();
-
         p5.beginShape();
-        // for (let i = 0; i < values.length; i++) {
-        //     const amplitude = values[i] as number;
-        //     const x = p5.map(i, 0, values.length - 1, 0, width);
-        //     const y = height / 2 + amplitude * height;
-        //     // Place vertex
-        //     p5.vertex(x, y);
-        // }
 
-        p5.rotateY(p5.frameCount * 0.01);
-        for (let j = 0; j < 5; j++) {
+        p5.rotateX(p5.frameCount * 0.6);
+        p5.rotateY(p5.frameCount * 0.6);
+        p5.rotateZ(p5.frameCount * 0.6);
+        for (let j = 0; j < 1; j++) {
             p5.push();
             let sum: number = 0;
             for (let i = 0; i < values.length; i++) {
@@ -39,12 +32,11 @@ export const WaveformVisualizer1 = new Visualizer(
                 sum += sum;
             }
 
-            for (let i = 0; i < 35; i++) {
-                // const size = values[i] as number;
+            for (let i = 0; i < 25; i++) {
                 p5.translate(
-                    p5.sin(sum * 70 * 0.001 + j) * 100,
-                    p5.sin(sum * 70 * 0.001 + j) * 100,
-                    i * 0.1
+                    p5.sin(sum * 90 * 0.01 + j) * 100,
+                    p5.sin(sum * 90 * 0.01 + j) * 100,
+                    i * 0.01
                 );
                 p5.rotateZ(p5.frameCount * 0.002);
                 p5.push();
