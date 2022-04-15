14aff65e87f49ca4234c02f0e9330e2fdc70f38b
Justin Lam <jlam18@mail.sfsu.edu>
2022-04-15 09:56:27 -0700

    Added canon in d

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -27,3 +27,10 @@ INSERT INTO artists (artist_id, artist_name) VALUES (1, "Bob Lee Swagger");
 INSERT INTO albums (album_id, album_title, artist_id) VALUES (1, "Shooter", 1);
 
 INSERT INTO songs (song_id, song_title, notes, artist_id, album_id) VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4 D4 D4', 1, 1);
+
+-- Justin's Addition
+INSERT INTO artists (artist_id, artist_name) VALUES (2, "Johann Pachelbel");
+
+INSERT INTO albums (album_id, album_title, artist_id) VALUES (2, "Pachelbel's Canon", 2);
+
+INSERT INTO songs (song_id, song_title, notes, artist_id, album_id) VALUES (2, 'Canon in D (for beginners)', 'C4 G4 A#4 E#4 F4 C4 F4 G4 C4 G4 A#4 E#4 F4 C4 F4 G4 C4 G4 A#4 E#4 C4 F4 G4 C4', 2, 2);
